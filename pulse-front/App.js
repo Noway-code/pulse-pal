afa3a4b0a9fe1f56335a2b6ae170dca6bff4c110
ChittebbayiPenugonda <cpenugonda3@gatech.edu>
2024-02-09 22:25:06 -0500

    activated the camera

--- pulse-front/App.js
+++ pulse-front/App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useState, useEffect, useRef} from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import {Camera, CameraType} from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  const [flash, setFlash] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () =>{
+      MediaLibrary.requestPermissionsAsync();
+      const cameraStatus = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+    })();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hi</Text>
+      <Camera 
+      style={styles.camera}
+      type={type}
+      flashMode = {flash}
+      ref={cameraRef}
+      >
+        <Text>in camera</Text>
+      </Camera>
+      <View>
+        
+      </View>
+      {(hasCameraPermission) ? (<Text>YA</Text>) : (<Text>Nah</Text>)}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +41,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  camera:{
+    width: Dimensions.get('window').width / 1.5,
+    height: Dimensions.get('window').height / 1.5,
+    borderRadius: 20,
+  }
 });
