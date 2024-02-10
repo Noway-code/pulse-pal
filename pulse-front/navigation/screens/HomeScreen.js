b020da15fe3746724bd28e65d8e969c82a615d51
noway <dragonfury1928@gmail.com>
2024-02-10 11:43:36 -0500

    Lib: Added React-Native-Paper

--- pulse-front/navigation/screens/HomeScreen.js
+++ pulse-front/navigation/screens/HomeScreen.js
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-paper';
 
 export default function HomeScreen({ navigation }) {
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 			<Text
 				onPress={() => alert('This is the "Home" screen.')}
-				style={{ fontSize: 26, fontWeight: 'bold' }}>Home Screen</Text>
+				style={{ fontSize: 26, fontWeight: 'bold' }}>
+					Home Screen
+			</Text>
+			<Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+				Press me
+			</Button>
 		</View>
 	);
 }
