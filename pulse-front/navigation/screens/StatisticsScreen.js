ff431ebea952b6227a2661f34653d6cac1d96d96
noway <dragonfury1928@gmail.com>
2024-02-10 01:59:22 -0500

    Add: Fixed API calls to frontend and test function

--- pulse-front/navigation/screens/StatisticsScreen.js
+++ pulse-front/navigation/screens/StatisticsScreen.js
@@ -1,12 +1,24 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import client from '../../api/client.js';
+import {useState, useEffect} from "react";
 
 export default function StatisticsScreen({navigation}) {
+	const [stats, setStats] = useState(null);
+
+	useEffect(() => {
+		client.get("test").then(
+			(response) => {
+				setStats(response.data);
+			}
+		)
+	}, []);
+
 	return (
 		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 			<Text
 				onPress={() => navigation.navigate('Home')}
-				style={{ fontSize: 26, fontWeight: 'bold' }}>Statistics Screen</Text>
+				style={{ fontSize: 26, fontWeight: 'bold' }}>Statistics Screen {stats}</Text>
 		</View>
 	);
 }
