b0962500cbeaa72c84937e6799d68fc645b5e2cd
Weldon Scott <wascott@uwaterloo.ca>
2024-02-09 23:43:13 -0500

    Added Axios client

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
 
 
-@app.route("/")
+@app.route("/test")
 def test():
     return "test"
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
--- pulse-front/App.js
+++ pulse-front/App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+
+import client from './api/client';
 
 export default function App() {
+  const [data, setData] = useState("none");
+
+  //this can be removed, just to show how client works
+  useEffect(() => {
+    client.get("test").then((response) => {
+      setData(response.data);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Test backend call: {data}</Text>
       <StatusBar style="auto" />
     </View>
   );
